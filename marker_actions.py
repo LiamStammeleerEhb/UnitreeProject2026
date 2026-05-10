c2239ecc6d77fb0f4fb297a7e8f805b1b2fea7b2
Liam Stammeleer <liam.stammeleer@student.ehb.be>
2026-05-10 21:06:07 +0200

    Update actions for markers in preperation of the presentation

--- marker_actions.py
+++ marker_actions.py
@@ -1,3 +1,11 @@
+# Possible functions
+
+#{ "type": "stop_walk" },
+#{ "type": "turn_degrees", "degrees": 90 },
+#{ "type": "pause", "seconds": 2.0 },
+#{ "type": "set_inactive" },
+#{ "type": "set_walk_speed", "speed": 0.5 }
+
 {
   "markers": [
     {
@@ -5,48 +13,51 @@
       "trigger_distance": 1.5,
       "actions": [
         { "type": "stop_walk" },
+        { "type": "pause", "seconds": 0.2 },
         { "type": "turn_degrees", "degrees": -90 },
+        { "type": "pause", "seconds": 0.2 },
         { "type": "set_walk_speed", "speed": 0.5 }
       ]
     },
     {
       "marker_id": 1,
-      "trigger_distance": 1.0,
+      "trigger_distance": 3.0,
       "actions": [
-        { "type": "stop_walk" },
-        { "type": "turn_degrees", "degrees": 90 },
-        { "type": "pause", "seconds": 2.0 },
-        { "type": "set_inactive" }
+        { "type": "set_walk_speed", "speed": 0.8 }
       ]
     },
     {
       "marker_id": 2,
-      "trigger_distance": 0.8,
+      "trigger_distance": 4,
       "actions": [
+        { "type": "set_walk_speed", "speed": 0.3 },
+        { "type": "pause", "seconds": 0.2 },
         { "type": "stop_walk" },
-        { "type": "turn_degrees", "degrees": -90 },
-        { "type": "pause", "seconds": 1.5 },
-        { "type": "set_walk_speed", "speed": 0.3 }
+        { "type": "pause", "seconds": 0.2 },
+        { "type": "turn_degrees", "degrees": 180 },
+        { "type": "pause", "seconds": 0.2 },
+        { "type": "set_walk_speed", "speed": 0.6 }
       ]
     },
     {
       "marker_id": 3,
       "trigger_distance": 3,
       "actions": [
-        { "type": "set_walk_speed", "speed": 0.7 }
+        { "type": "stop_walk" },
+        { "type": "pause", "seconds": 0.2 },
+        { "type": "turn_degrees", "degrees": 90 },
+        { "type": "pause", "seconds": 0.2 },
+        { "type": "set_walk_speed", "speed": 0.5 }
       ]
     },
     {
       "marker_id": 4,
-      "trigger_distance": 3,
+      "trigger_distance": 2,
       "actions": [
         { "type": "set_walk_speed", "speed": 0.3 },
-        { "type": "pause", "seconds": 0.5 },
+        { "type": "pause", "seconds": 0.2 },
         { "type": "stop_walk" },
-        { "type": "pause", "seconds": 0.5 },
-        { "type": "turn_degrees", "degrees": 180 },
-        { "type": "pause", "seconds": 0.5 },
-        { "type": "set_walk_speed", "speed": 0.6 }
+        { "type": "set_inactive" },
       ]
     }
   ]
