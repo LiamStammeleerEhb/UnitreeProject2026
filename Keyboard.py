79e4e6c7f15dbc0e22a6fb284cb7fa37c02e66ad
F0RD-P1N3S15 <bridget.parish@student.ehb.be>
2026-05-11 10:45:43 +0200

    Enhance keyboard controls
    Added/updated controls.

--- Keyboard.py
+++ Keyboard.py
@@ -5,22 +5,27 @@ import sys
 import tty
 import termios
 import threading
+import random
+import time
 
 from unitree_webrtc_connect.webrtc_driver import (
     UnitreeWebRTCConnection,
     WebRTCConnectionMethod
 )
-from unitree_webrtc_connect.constants import RTC_TOPIC, SPORT_CMD
+from unitree_webrtc_connect.constants import RTC_TOPIC, SPORT_CMD, VUI_COLOR
 
 logging.basicConfig(level=logging.FATAL)
 
 MOVE_SPEED = 0.5
 TURN_SPEED = 1
 BRIGHTNESS_LVL = 1
+DISCO_COLOURS = [VUI_COLOR.RED, VUI_COLOR.YELLOW, VUI_COLOR.GREEN, VUI_COLOR.CYAN, VUI_COLOR.BLUE, VUI_COLOR.PURPLE]
 
 def print_controls():
     print("""
 ================= CONTROLS =================
+   THE BASICS
+-----------------
 z : vooruit
 s : achteruit
 a : draaien links
@@ -29,10 +34,30 @@ q : zijwaarts links
 d : zijwaarts rechts
 o : opstaan
 l : neerliggen
+-----------------
+ EXTRA MOVEMENTS
+-----------------
 h : hello
 x : stretch
-i : light on
-u : light off
+-----------------
+ LIGHT CONTROLS
+-----------------
+u : zaklamp uit
+i : zaklamp aan
+    1. White        🤍
+    2. Red          ❤️
+    3. Yellow       💛
+    4. Green        💚
+    5. Cyan         🩵
+    6. Blue         💙 
+    7. Purple       💜 
+    8. Disco        🪩 
+    9. Flash        🔦 
+    0. Licht uit    🖤
+-----------------
+  HELP CONTROLS
+-----------------
+r : EMERGENCY STOP
 p : print controls
 (ctrl + c) * 2 : exit
 ============================================
@@ -72,34 +97,42 @@ def keyboard_listener(conn, loop, stop_event):
             stop_event.set()
             break
 
+        # Vooruit
         if k == "z":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, x=MOVE_SPEED), loop)
 
+        # Achteruit
         elif k == "s":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, x=-MOVE_SPEED), loop)
 
+        # Draaien links
         elif k == "a":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, z=TURN_SPEED), loop)
 
+        # Draaien rechts
         elif k == "e":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, z=-TURN_SPEED), loop)
 
+        # Zijwaards links
         elif k == "q":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, y=MOVE_SPEED), loop)
 
+        # Zijwaards rechts
         elif k == "d":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, y=-MOVE_SPEED), loop)
 
+        # /
         elif k == " ":
             asyncio.run_coroutine_threadsafe(
                 send_move(conn, x=0), loop)
 
+        # Hello
         elif k == "h":
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
@@ -107,6 +140,7 @@ def keyboard_listener(conn, loop, stop_event):
                     {"api_id": SPORT_CMD["Hello"]}
                 ), loop)
 
+        # Stretch
         elif k == "x":
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
@@ -114,6 +148,7 @@ def keyboard_listener(conn, loop, stop_event):
                     {"api_id": SPORT_CMD["Stretch"], "parameter": {"data": False}}
                 ), loop)
 
+        # Neerliggen
         elif k == "l":
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
@@ -121,6 +156,7 @@ def keyboard_listener(conn, loop, stop_event):
                     {"api_id": SPORT_CMD["StandDown"]}
                 ), loop)
 
+        # Opstaan
         elif k == "o":
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
@@ -128,6 +164,7 @@ def keyboard_listener(conn, loop, stop_event):
                     {"api_id": SPORT_CMD["RecoveryStand"], "parameter": {"data": False}}
                 ), loop)
 
+        # Zaklamp aan
         elif k == "i":
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
@@ -135,6 +172,77 @@ def keyboard_listener(conn, loop, stop_event):
                     {"api_id": 1005, "parameter": {"brightness": BRIGHTNESS_LVL}}
                 ), loop)
 
+        # White
+        elif k == "1":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.WHITE}}
+                ), loop)
+        # Red
+        elif k == "2":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.RED}}
+                ), loop)
+        # Yellow
+        elif k == "3":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.YELLOW}}
+                ), loop)
+        # Green
+        elif k == "4":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.GREEN}}
+                ), loop)
+        # Cyan
+        elif k == "5":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.CYAN}}
+                ), loop)
+        # Blue
+        elif k == "6":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.BLUE}}
+                ), loop)
+        # Purple
+        elif k == "7":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.PURPLE}}
+                ), loop)
+        # Disco
+        elif k == "8":
+            for i in range(30):
+                select_disco_colour = random.choice(DISCO_COLOURS)
+
+                asyncio.run_coroutine_threadsafe(
+                    conn.datachannel.pub_sub.publish_request_new(
+                        RTC_TOPIC["VUI"],
+                        {"api_id": 1007, "parameter": {"color": select_disco_colour}}
+                    ), loop)
+                time.sleep(0.2)
+        # Flash
+        elif k == "9":
+            asyncio.run_coroutine_threadsafe(
+                conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["VUI"],
+                    {"api_id": 1007, "parameter": {"color": VUI_COLOR.WHITE, "flash_cycle": 500}}
+                ), loop)
+        # Licht uit
+        #elif k == "0":
+
+        # Zaklamp uit
         elif k == "u":
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
@@ -142,6 +250,12 @@ def keyboard_listener(conn, loop, stop_event):
                     {"api_id": 1005, "parameter": {"brightness": 0}}
                 ), loop)
 
+        # EMERGENCY STOP
+        elif k == "r":
+            asyncio.run_coroutine_threadsafe(
+                send_move(conn, x=0, z=0), loop)
+
+        # Print controls
         elif k == "p":
             print_controls()
 
@@ -149,7 +263,7 @@ def keyboard_listener(conn, loop, stop_event):
 async def main():
     conn = UnitreeWebRTCConnection(
         WebRTCConnectionMethod.LocalSTA,
-        ip="unitree.local"
+        ip="10.2.172.107"
     )
 
     await conn.connect()
